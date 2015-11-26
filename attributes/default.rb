7ecf0c05b521c90134299c8a2ff7eadec7764ae2
polidog <polidogs@gmail.com>
2015-11-26 18:54:47 +0900

    php+fpm+nginx

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['php']['fpm_user']      = 'nginx'
+default['php']['fpm_group']     = 'nginx'
+default['php-fpm']['user'] = 'nginx'
+default['php-fpm']['group'] = 'nginx'
+default['php']['packages'] = %w(php php-devel php-cli php-pear php-fpm)
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,4 +4,30 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+# php install
+include_recipe 'php::default'
+
+# nginx install
 include_recipe 'nginx::default'
+
+# fpm install
+node.default['php-fpm']['pools'] = {
+  "www" => {
+    :enable => "true",
+    :max_requests => 5000,
+    :php_options => { 'php_admin_flag[log_errors]' => 'on', 'php_admin_value[memory_limit]' => '128M' }
+  }
+}
+include_recipe 'php-fpm::default'
+
+
+
+nginx_site 'default' do
+  enable true
+  template "nginx.erb"
+  variables({
+    :domain => 'hage.jp',
+    :document_root => '/var/www/app',
+    :socket => 'unix:/var/run/php-fpm-www.sock'
+  })
+end
